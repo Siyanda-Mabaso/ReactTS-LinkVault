1e1b4d54277b2a325a9661a027bca1d354a7c051
Syanda-Mabaso <syandamabaso39@gmail.com>
2026-07-22 10:58:28 +0200

    did the header component

---
+++ linkvault/src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.css'
+
+export const Header = () => {
+  return (
+    <header className='header' >
+        <h1>Link Vaults</h1>
+    </header>
+  )
+}
